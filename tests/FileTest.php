b8ce8c65595e85c2cf53db83c3f15765159c6eb7
Alex <t_tapak@yahoo.com>
2016-05-06 22:02:19 +0300

    Add test for exists method

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * File class tests tests.
- * @package     File
- * @version     1.0.1
+ * @package     Tapakan\File
+ * @version     1.0.2
  * @license     http://mit-license.org/
  * @author      Tapakan https://github.com/Tapakan
  * @coder       Alexander Oganov <t_tapak@yahoo.com>
@@ -21,5 +21,14 @@ class FileTests extends \PHPUnit_Framework_TestCase
     public function testGetExt()
     {
         $this->assertEquals('php', File::getExt(__FILE__));
+    }
+
+    public function testExists()
+    {
+        $true  = File::exists(__DIR__ . '/bootstrap.php');
+        $false = File::exists(__DIR__ . '/autoload.php');
+
+        $this->assertEquals(true, $true);
+        $this->assertEquals(false, $false);
     }
 }
